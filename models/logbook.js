91364b28301d63f889d626b698ed24bf9d2ff0f9
Utkrisht <utkrishttrivedi@protonmail.com>
2023-03-27 12:08:07 +0530

    first test commit

--- models/logbook.js
+++ models/logbook.js
@@ -1,11 +1,9 @@
 const mongoose =require('mongoose');
-
-
 const Schema=mongoose.Schema;
 
 const logSchema=new Schema({
     trainer:{type:String},
-    trainy:{type:String,unique:true},
+    trainee:{type:String,unique:true},
     drone:{type:String,required:true},
     startDate:{type:String,required:true},
     endDate:{type:String,required:true},
@@ -13,4 +11,5 @@ const logSchema=new Schema({
     create_at:{type:Number,default:Date.now().valueOf()},
     updated_at:{type:Number,default:Date.now().valueOf()}
 })
+
 module.exports=mongoose.model('logbook',logSchema);
--- routes/auth_route.js
+++ routes/auth_route.js
@@ -1,50 +1,50 @@
 const router = require("express").Router();
-
 const Logboook = require('../models/logbook');
 const User = require('../models/user');
 
- router.post('/register', (req, res) => {
- //  res.json("register work" );
-    const user = new User({
-                        displayName: req.body.displayName,
-                       email: req.body.email,
-                        password: req.body.password,
-                    })
-                    user.save()
-                                    .then((_) => {
-                                       res.json({ success: true, message: "Account has been created" })
-                                    })
-                                    .catch((err) => {
-                                                          if (err.code === 11000) {
-                                                            return res.json({ uccses: false, message: "Email is Already exist ! " })
-                                                          }
-                                                            res.json({ succses: false, message: "Authentication failed" })
-                                                     })
+router.post('/register', (req, res) => {
+//  res.json("register work" );
+   const user = new User({
+      displayName: req.body.displayName,
+      email: req.body.email,
+      password: req.body.password,
+   })
+   user.save()
+   .then((_) => {
+      res.json({ success: true, message: "Account has been created" })
+   })
+   .catch((err) => {
+      if (err.code === 11000) {
+         return res.json({ uccses: false, message: "Email is Already exist ! " })
+      }
+      res.json({ succses: false, message: "Authentication failed" })
+   })
 });
 
 router.post('/login', (req, res) => {
-   // res.json( "login" );
+// res.json( "login" );
 });
 
 router.post('/logbook', (req, res) => {
-    //res.json("logbbok work" );
-     const loguser = new Logbook({
-                        trainer: req.body.Trainer,
-                        trainy: req.body.Trainy,
-                        drone: req.body.Drone,
-                        startDate: req.body.Drone,
-                        endDate: req.body.Drone,
-                        duration: req.body.Drone,
-                     })
-                     loguser.save()
-                                     .then((_) => {
-                                        res.json({ success: true, message: "Log has been created" })
-                                     })
-                                     .catch((err) => {
-                                                           if (err.code === 11000) {
-                                                             return res.json({ succses: false, message: "Trainy is Already exist ! " })
-                                                           }
-                                                             res.json({ succses: false, message: "Authentication failed" })
-                                                      })
- });
+//res.json("logbbok work" );
+   const loguser = new Logbook({
+      trainer: req.body.Trainer,
+      trainee: req.body.Trainee,
+      drone: req.body.Drone,
+      startDate: req.body.Drone,
+      endDate: req.body.Drone,
+      duration: req.body.Drone,
+   })
+   loguser.save()
+   .then((_) => {
+      res.json({ success: true, message: "Log has been created" })
+   })
+   .catch((err) => {
+      if (err.code === 11000) {
+         return res.json({ succses: false, message: "Trainy is Already exist ! " })
+      }
+      res.json({ succses: false, message: "Authentication failed" })
+   })
+});
+
 module.exports = router
